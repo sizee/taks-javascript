9c8a3a682c83d24fe17ce5ec8f2f691b50ecaf93
sizee <994043269@qq.com>
2016-07-19 16:54:41 +0800

    桌游精灵

---
+++ 桌游精灵-1.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Fly-mydream on 16.7.19.
+ */
+function mytime(){
+    var mytime=new Date();
+    var h=mytime.getHours();
+    var m=mytime.getMinutes();
+    var s=mytime.getSeconds();
+    var hh="";
+    m=check(m);
+    s=check(s);
+    if(h>12)
+    {
+        h=h%12;
+        hh="PM";
+
+    }
+         else
+       hh="AM";
+    document.getElementById('mytimes').innerHTML=h+":"+m+":"+s+" "+hh;
+
+    setTimeout('mytime()',500);
+}
+function check(i){
+    if(i<10) {
+        i="0"+i;
+    } 
+    return i;
+}
