26ed7e9fcf975a4742d6c852976ef0f74d440b94
sizee <994043269@qq.com>
2016-07-20 08:24:21 +0800

    精灵

--- 桌游精灵-1.js
+++ 桌游精灵-1.js
@@ -20,7 +20,7 @@ function mytime(){
     setTimeout('mytime()',500);
 }
 function back(){
-    window.location.href="桌游精灵-首页.html";
+    window.location.href="https://raw.githubusercontent.com/sizee/taks-javascript/master/%E6%A1%8C%E6%B8%B8%E7%B2%BE%E7%81%B5-%E9%A6%96%E9%A1%B5.html";
 }
 function check(i){
     if(i<10) {
