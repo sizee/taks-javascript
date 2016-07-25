81aa76ba9d18977ac95832eacd4f680469a8733d
sizee <994043269@qq.com>
2016-07-25 16:38:47 +0800

    精灵-参数

---
+++ list-js/翻牌.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Fly-mydream on 16.7.25.
+ */
+$(function() {
+    var hello=new Array();
+    for(var i=0;i<localStorage.length;i++)
+    {
+        hello[i]=localStorage.getItem("array");
+    }
+})
+  
