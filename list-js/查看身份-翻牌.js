8dff09637cf2ffc5716810ecc04a7cac0a2d6ec9
sizee <994043269@qq.com>
2016-07-26 12:04:04 +0800

    精灵-参数

--- list-js/查看身份-翻牌.js
+++ list-js/查看身份-翻牌.js
@@ -2,24 +2,25 @@
  * Created by Fly-mydream on 16.7.25.
  */
 $(function() {
-    var hello=new Array();
+    var hello=new Array(localStorage.length);
     var but=$(".content button");
     var number=$(".content .number");
     var content_top_one=$(".content .content-top-one");
     var content_top_two=$(".content .content-top-two");
     var content_top_two_h2=$(".content-top-two h2");
-    var content_top_one_recting=$(".content .content-top-one .recting .number");
-    var content_top_two_recting=$(".content .content-top-two .recting .number");
 
     var hello=JSON.parse(localStorage.getItem('array'));
-   /* console.log(hello,hello.length);
-    alert(hello[0]);
-   alert(hello[1]);
-    alert(hello[2]);alert(hello[3]);alert(hello[4]);alert(hello[5]);*/
+    
      var littlenumber=0;
      var somenumber=2;
      number.text(1);
+    
     but.bind("click",function(){
+        if(littlenumber==hello.length){
+            window.location.href="桌游精灵-流程.html";
+            but.unbind();
+            return false;
+        }
          if(content_top_one.is(":visible"))
          {
              content_top_one.css("display","none");
@@ -27,16 +28,23 @@ $(function() {
              content_top_two_h2.text("角色:"+ hello[littlenumber]);
              but.text("隐藏并传递给"+somenumber+"号");
              littlenumber++;
-             somenumber++;
          }
         else{
              content_top_one.css("display","block");
              content_top_two.css("display","none");
-             content_top_one_recting.text(somenumber);
-             content_top_two_recting.text(somenumber);
-
+             but.text("查看"+somenumber+"号身份");
+             number.text(somenumber);
+             somenumber++;
          }
-        
-    })
-})
+        if(littlenumber==hello.length){
+            littlenumber=hello.length;
+            somenumber=hello.length;
+            console.log(littlenumber);
+            but.text("法官日记");
+        }
+    });
+    
+
+    
+});
   
