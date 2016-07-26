98c89d1a2ae58fa8bbbcfdaa1a21b710dcbdb64c
sizee <994043269@qq.com>
2016-07-26 13:47:30 +0800

    精灵-参数

--- list-js/法官日记.js
+++ list-js/法官日记.js
@@ -5,10 +5,12 @@ $(function(){
     var number=new Array();
         number=JSON.parse(localStorage.getItem("array"));
     var boxnum=new Array(number.length);
+    var but=$(".but");
    for(var i=1;i<number.length;i++){
-
        $(".content .wrp").append($(".content .wrp .box:first").clone(true));
    }
-
+    but.click(function(){
+        location.href="桌游精灵-流程.html";
+    })
 
 })
